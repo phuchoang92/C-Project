5953d32513a82ace4dd640c1773dc313ace747c3
phuchoang29 <changchayabcd@gmail.com>
2022-08-13 19:28:34 +0700

    Add files via upload

---
+++ NhanVienWebApp/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NhanVienAPI.Models;
+using NhanVienWebApp.Models;
+using System.Diagnostics;
+using System.Text;
+
+namespace NhanVienWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("Access_token") == null)
+            {
+                return Redirect("~/Login/Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Login(LoginUser userInfo)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var jsonObject = JsonConvert.SerializeObject(userInfo);
+                StringContent stringContent = new StringContent(jsonObject,Encoding.UTF8,"application/json");
+                using (var response = await httpClient.PostAsync("https://localhost:7215/api/token", stringContent)) 
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        TempData["StatusMessage"] = "Login Fail! Check your username or password";
+                        return Redirect("~/Login/Index");
+                    }
+                    else
+                    {
+                        string token = await response.Content.ReadAsStringAsync();
+                        if (token == "Invalid Credentials")
+                        {
+                            ViewBag.Message = "Incorrect Usename or Password";
+                            return Redirect("~/Home/Index");
+                        }
+                        HttpContext.Session.SetString("Access_token", token);
+                    }                 
+                }
+            }
+            return RedirectToAction("Index", "NhanVien");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NhanVienWebApp/Controllers/NhanVienController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace NhanVienWebApp.Controllers
+{
+    [Route("NhanVien")]
+    public class NhanVienController : Controller
+    {
+
+        
+        public static string BaseUrl = "https://localhost:7215/api/NhanVien";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Report")]
+        public async Task<IActionResult> GetReport()
+        {
+            var accessToken = HttpContext.Session.GetString("Access_token");
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                var url = BaseUrl;
+                HttpClient client = new HttpClient();
+                HttpResponseMessage file = new HttpResponseMessage();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                var response = await client.PostAsync(url, null);
+                
+                return File(response.Content.ReadAsByteArrayAsync().Result,"application/pdf");
+            }
+            return Unauthorized();
+        }
+    }
+}
