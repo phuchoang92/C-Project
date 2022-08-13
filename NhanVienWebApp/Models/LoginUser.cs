90abd91110bb2a0be28063640f3f7e546f84337c
phuchoang29 <changchayabcd@gmail.com>
2022-08-13 19:31:08 +0700

    Add files via upload

---
+++ NhanVienWebApp/Models/LoginUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NhanVienAPI.Models
+{
+    public class LoginUser
+    {
+        public string UserId { get; set; } = "unknown";
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
